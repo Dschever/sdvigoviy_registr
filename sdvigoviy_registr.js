83fe43b757d1b0156e3ad9aeea3bff4b45f06c8b
Dschever <ds_archangel_@mail.ru>
2016-12-03 01:45:17 +0500

    Update sdvigoviy_registr.js

--- sdvigoviy_registr.js
+++ sdvigoviy_registr.js
@@ -1,16 +1,16 @@
 var SuperRegistr = function(opts, i, tr){
 	opts = opts || {};
-    i = i || 1;
-    tr = tr || true;
-    this._arr_pin = CreatePin(i, this, tr);
-	  this._ds = opts.ds;
-	  this._st = opts.st;
+    	i = i || 1;
+   	tr = tr || true;
+    	this._arr_pin = create_pin(i, this, tr);
+	this._ds = opts.ds;
+	this._st = opts.st;
   	this._sh = opts.sh;
-	  this._mr = opts.mr;
+	this._mr = opts.mr;
   	this._oe = opts.oe;
-	  this._hi_z = false;
-    if(this._mr!=undefined) this._mr.write(1);
-    if(this._oe!=undefined) this._oe.write(0);
+	this._hi_z = false;
+    	if(this._mr!=undefined) this._mr.write(1);
+   	if(this._oe!=undefined) this._oe.write(0);
 };
 var SuperPin = function(p, registr){
   this._pin = p;
@@ -39,27 +39,27 @@ SuperRegistr.prototype.PinUpdate = function (){
 };
 
 SuperPin.prototype.write = function(on_off){
-  			if(on_off == true){
-  				this._registr._arr_pin[this._pin]=1;
-  				this._registr.PinUpdate();
-  			}else if(on_off == false){
+	if(on_off == true){
+		this._registr._arr_pin[this._pin]=1;
+		this._registr.PinUpdate();
+	}else if(on_off == false){
                 this._registr._arr_pin[this._pin]=0;
-  				this._registr.PinUpdate();
-  			}else{
-  				console.log("Error    write(true) or false, 1,0");
-  			}
+		this._registr.PinUpdate();
+	}else{
+		console.log("Error    write(true) or false, 1,0");
+	}
 };
 
 SuperRegistr.prototype.Reset = function (){
   if(this._mr!=undefined){
-    this._mr.write(0);
+    	this._mr.write(0);
 	this._st.write(1);
 	this._st.write(0);
-    this._mr.write(1);
+    	this._mr.write(1);
   }else if(this._mr==undefined){
   	for (var i=0; this._arr_pin.length>i; i++) {
-  		this._arr_pin[i]=0;
-  	};
+  		this._arr_pin[i] = 0;
+  	}
   	this._st.write(1);
 	this._st.write(0);
   }else{
@@ -67,27 +67,27 @@ SuperRegistr.prototype.Reset = function (){
   }
 };
 
-SuperRegistr.prototype.HI_Z = function (){
-if(this._mr!=undefined){
-	if(this._hi_z == false){
-		this._hi_z = true;
-		this._oe.write(1);
-	}else if(this._hi_z == true){
-		this._hi_z = false;
-		this._oe.write(0);
+SuperRegistr.prototype.Hi_z = function (){
+	if(this._mr!=undefined){
+		if(this._hi_z == false){
+			this._hi_z = true;
+			this._oe.write(1);
+		}else if(this._hi_z == true){
+			this._hi_z = false;
+			this._oe.write(0);
+		}
+	}else{
+		console.log("Error  | oe |  Error");
 	}
-}else{
-	console.log("Error  | oe |  Error");
-}
 };
 
-function CreatePin(i, registr, tr){
-	var arr=[];
+function create_pin(i, registr, tr){
+	var arr_pin=[];
 	for(p=0;p<i*8;p++){
-        arr.push(0);
-  		if(tr == true)global["R"+p] = new SuperPin(p, registr); 
+        	arr_pin.push(0);
+  		if(tr == true)global["Q"+p] = new SuperPin(p, registr); 
 	}
-    return arr;
+	return arr_pin;
 };
 
 exports.connect = function(opts, i, tr){
